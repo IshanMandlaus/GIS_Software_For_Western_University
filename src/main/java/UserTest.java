e1ff54da28377beb9ac967468045199adcc74033
mansar33 <mansar33@uwo.ca>
2023-04-09 23:39:21 -0400

    Added JavaDocs for UserTest.java

--- src/main/java/UserTest.java
+++ src/main/java/UserTest.java
@@ -7,52 +7,78 @@ import java.io.IOException;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+/**
+ * Tests the User class
+ * @author Muhammad Ansari
+ * @version 1.0
+ */
+
 class UserTest {
 
+    /**
+     * Tests the addToFavourites method in the User class
+     */
     @Test
     public void testAddToFavourites() {
+        /** Create a new map */
         JLabel map = new JLabel();
         JPanel mapContainer = new JPanel();
         map.setBounds(0, 0, 1920, 1080);
         mapContainer.add(map, 0);
 
+        /** Create a new user and a new POI */
         User user = new User("mansar33", "password");
         POI pointOfInterest = new POI(true, "Test", 123, "Test description", 3, 4, 6, 0.5, 0.5, map, null, null, null);
+
+        /** Add the POI to the user's favourites */
         user.addToFavourites(pointOfInterest);
 
+        /** Check that the POI has been added to the user's favourites */
         Assertions.assertEquals(1, user.favourites.size());
         Assertions.assertEquals(pointOfInterest, user.favourites.get(0));
-
     }
-//j
+
+    /**
+     * Tests the createPOI method in the User class
+     */
     @Test
     public void testCreatePOI() {
+        /** Create a new map */
         User user = new User("mansar33", "password");
         JLabel map = new JLabel();
         JPanel mapContainer = new JPanel();
         map.setBounds(0, 0, 1920, 1080);
         mapContainer.add(map, 0);
 
+        /** Create a new POI */
         POI pointOfInterest = new POI(true, "Test", 123, "Test description", 3, 4, 6, 0.5, 0.5, map, null, null, null);
         user.createPOI(pointOfInterest);
 
+        /** Check that the POI has been added to the user's POIs */
         Assertions.assertEquals(1, user.pointsOfInterest.size());
         Assertions.assertEquals(pointOfInterest, user.pointsOfInterest.get(0));
     }
 
+    /**
+     * Tests the deletePOI method in the User class
+     */
     @Test
     public void testDeletePOI() {
+        /** Create a new map */
         User user = new User("mansar33", "password");
         JLabel map = new JLabel();
         JPanel mapContainer = new JPanel();
         map.setBounds(0, 0, 1920, 1080);
         mapContainer.add(map, 0);
 
+        /** Create a new POI */
         POI poi = new POI(true, "Test", 123, "Test description", 3, 4, 6, 0.5, 0.5, map, null, null, null);
 
+        /** Add the POI to the user's POIs and then delete it */
         user.createPOI(poi);
         user.deletePOI(poi);
 
+        /** Check that the POI has been deleted from the user's POIs */
         Assertions.assertEquals(0, user.pointsOfInterest.size());
     }
 
@@ -97,6 +123,7 @@ class UserTest {
         User user = new User("mansar33", "password");
         String username = user.getUsername();
 
+        /** Check that the username is correct */
         Assertions.assertEquals("mansar33", username);
     }
 }
